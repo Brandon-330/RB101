45883c76a830608c32e141dbfbd7f9c0fdd68b16
Brandon Lima <112784850+Brandon-330@users.noreply.github.com>
2023-01-04 01:22:49 +0000

    renamed:    easy 2/exercise1.rb -> easy2/exercise1.rb 	renamed:    easy 2/exercise10.rb -> easy2/exercise10.rb 	renamed:    easy 2/exercise2.rb -> easy2/exercise2.rb 	renamed:    easy 2/exercise3.rb -> easy2/exercise3.rb 	renamed:    easy 2/exercise4.rb -> easy2/exercise4.rb 	renamed:    easy 2/exercise5.rb -> easy2/exercise5.rb 	renamed:    easy 2/exercise6.rb -> easy2/exercise6.rb 	renamed:    easy 2/exercise7.rb -> easy2/exercise7.rb 	renamed:    easy 2/exercise8.rb -> easy2/exercise8.rb 	renamed:    easy 2/exercise9.rb -> easy2/exercise9.rb 	new file:   easy3/exercise1.rb 	deleted:    test

---
+++ easy3/exercise1.rb
@@ -0,0 +1,22 @@
+arr = []
+index = 1
+
+6.times do
+    place = index.to_s + "st" if index == 1
+    place = index.to_s + "nd" if index == 2
+    place = index.to_s + "rd" if index == 3
+    place = index.to_s + "th" if index > 3
+    place = "last" if index == 6 
+    
+    puts "==> Enter the #{place} number: "
+    arr << gets.chomp.to_i
+    index += 1
+end
+
+num = arr[-1]
+arr.pop
+
+bool = false
+arr.each { |x| bool = true if x == num }
+
+puts bool ? "The number #{num} appears in #{arr}." : "The number #{num} does not appear in #{arr}."
