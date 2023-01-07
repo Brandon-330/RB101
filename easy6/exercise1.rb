26ee1394a26e929fac64092806e19259baa4882d
Brandon Lima <112784850+Brandon-330@users.noreply.github.com>
2023-01-07 21:40:34 +0000

    new file:   easy6/exercise1.rb

---
+++ easy6/exercise1.rb
@@ -0,0 +1,22 @@
+DEGREE = '\xC2\xB0'
+MINUTES_IN_HOUR = 60
+SECONDS_IN_MINUTE = 60
+
+def dms(n)
+  if n.to_s.include?('.')
+    degrees, decimal = n.to_s.split('.')
+    minutes = (decimal.to_f / 100) * MINUTES_IN_HOUR
+    minutes, decimal = minutes.to_s.split('.')
+    seconds = (seconds.to_f / 100) * SECONDS_IN_MINUTE
+    "%(#{degrees}#{DEGREE}#{format('%02d', minutes)}\'#{format('%02d', seconds)}\")"
+  else
+    "%(#{n}#{DEGREE}00\'00\")"
+  end
+end
+
+p dms(30) #== %(30°00'00")
+p dms(76.73) == %(76°43'48")
+p dms(254.6) == %(254°36'00")
+p dms(93.034773) == %(93°02'05")
+p dms(0) == %(0°00'00")
+p dms(360) == %(360°00'00") || dms(360) == %(0°00'00")
