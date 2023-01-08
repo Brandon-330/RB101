ce4d0ea9beee58ea26f280394ccc3b1103375340
Brandon Lima <112784850+Brandon-330@users.noreply.github.com>
2023-01-08 20:44:06 +0000

    modified:   lesson_2/rock_paper_scissors.rb

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -3,33 +3,68 @@
 # Create a method which picks 1 element from a rock, paper, scissor array
 # if statements to determine who won
 
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = %w(rock paper scissors spock lizard)
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def player_choice(choice)
+  choice.downcase!
+  if choice.start_with?('r')
+    "rock"
+  elsif choice.start_with?('p')
+    "paper"
+  elsif choice.start_with?('l')
+    "lizard"
+  elsif choice.start_with?('sc')
+    "scissors"
+  elsif choice.start_with?('sp')
+    "spock"
+  else
+    nil
+  end
+end
+
+
 def win?(first, second)
   (first == 'rock' && second == 'scissors') ||
+    (first == 'rock' && second == 'lizard') ||
     (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+    (first == 'paper' && second == 'spock') ||
+    (first == 'scissors' && second == 'paper') ||
+    (first == 'scissors' && second == 'lizard') ||
+    (first == 'spock' && second == 'scissors') ||
+    (first == 'spock' && second == 'rock') ||
+    (first == 'lizard' && second == 'paper') ||
+    (first == 'lizard' && second == 'spock')
 end
 
 def display_results(player, computer)
   if win?(player, computer)
-    prompt("You won!")
+    prompt("You won the round!")
   elsif win?(computer, player)
-    prompt("You lost!")
+    prompt("You lost the round!")
   else
-    prompt("It is a tie!")
+    prompt("Tied round!")
   end
 end
 
+def scoreboard(player, computer, player_wins, computer_wins)
+  if win?(player, computer)
+    player_wins += 1
+  elsif win?(computer, player)
+    computer_wins += 1
+  end
+  [player_wins, computer_wins]
+end
+
+player_wins, computer_wins = 0, 0
 loop do
   choice = nil
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(", ")}")
-    choice = Kernel.gets().chomp()
+    prompt("Choose one: #{VALID_CHOICES.join(", ")} (or type 'r', 'p', 'l', 'sc', or 'sp')")
+    choice = player_choice(Kernel.gets().chomp())
 
     if VALID_CHOICES.include?(choice)
       break
@@ -43,9 +78,14 @@ loop do
   prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
 
   display_results(choice, computer_choice)
-  
-  prompt("Would you like to play again? ")
-  break unless Kernel.gets().chomp().downcase().start_with?('y')
+
+  player_wins, computer_wins = scoreboard(choice, computer_choice, player_wins, computer_wins)
+  if player_wins == 3
+    prompt("The grand winner is you!")
+  elsif computer_wins == 3
+    prompt("The grand winner is the computer!")
+  end
+  break if player_wins == 3 || computer_wins == 3
 end
 
 prompt("Thank you for playing. Have a great day!")
