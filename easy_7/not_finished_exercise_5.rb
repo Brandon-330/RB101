822b91630de382090d7c2f785e47dda34ea35ea8
Brandon Lima <112784850+Brandon-330@users.noreply.github.com>
2023-01-10 02:27:04 +0000

    new file:   easy_7/not_finished_exercise_4.rb 	new file:   easy_7/not_finished_exercise_5.rb

---
+++ easy_7/not_finished_exercise_5.rb
@@ -0,0 +1,7 @@
+def staggered_case(s)
+  
+end
+
+staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+staggered_case('ALL_CAPS') == 'AlL_CaPs'
+staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
