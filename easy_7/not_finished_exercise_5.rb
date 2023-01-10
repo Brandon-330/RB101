22f8749a82659f9f8038264af020efd7add7a7a2
Brandon Lima <112784850+Brandon-330@users.noreply.github.com>
2023-01-10 02:58:54 +0000

    modified:   easy_7/not_finished_exercise_5.rb

--- easy_7/not_finished_exercise_5.rb
+++ easy_7/not_finished_exercise_5.rb
@@ -1,7 +1,15 @@
 def staggered_case(s)
-  
+  new_s = ''
+  s.each_char.with_index do |char, ind| 
+    if !!char.match(/[a-zA-Z]/) && ind.odd?
+      new_s << char.swapcase
+    else
+      new_s << char
+    end
+  end
+  p new_s
 end
 
-staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
-staggered_case('ALL_CAPS') == 'AlL_CaPs'
-staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
+p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+p staggered_case('ALL_CAPS') == 'AlL_CaPs'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
