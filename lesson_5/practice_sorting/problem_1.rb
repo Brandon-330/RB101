89224e5e452e0b4d88d8a28ed73c8b5d3ad5bfb5
Brandon Lima <112784850+Brandon-330@users.noreply.github.com>
2023-01-14 00:17:57 +0000

    new file:   lesson_5/practice_sorting/problem_1.rb 	new file:   lesson_5/practice_sorting/problem_2.rb 	new file:   lesson_5/practice_sorting/problem_3.rb 	new file:   lesson_5/practice_sorting/problem_4.rb 	new file:   lesson_5/practice_sorting/problem_5.rb

---
+++ lesson_5/practice_sorting/problem_1.rb
@@ -0,0 +1,3 @@
+arr = ['10', '11', '9', '7', '8']
+
+p arr.map { |el| el.to_i }.sort.reverse
---
+++ lesson_5/practice_sorting/problem_5.rb
@@ -0,0 +1,13 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+sum = 0
+munsters.each do |hash1, hash2|
+  sum += hash2.select { |keys| keys[:gender]  }
+
+p sum
