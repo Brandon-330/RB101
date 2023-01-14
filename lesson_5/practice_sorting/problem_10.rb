4005ec4e3f30004a2d29e1ded14f5629d0bfd076
Brandon Lima <112784850+Brandon-330@users.noreply.github.com>
2023-01-14 19:02:07 +0000

    modified:   lesson_5/practice_sorting/problem_10.rb 	new file:   lesson_5/practice_sorting/problem_11.rb 	new file:   lesson_5/practice_sorting/problem_12.rb

--- lesson_5/practice_sorting/problem_10.rb
+++ lesson_5/practice_sorting/problem_10.rb
@@ -1,9 +1,9 @@
 arr = [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}]
 
-arr.map! do |hash|
+new_arr = arr.map do |hash|
   hash.map do |k, v|
     k, v = k, v + 1
-  end
+  end.to_h
 end
 
-p arr
+p new_arr
---
+++ lesson_5/practice_sorting/problem_12.rb
@@ -0,0 +1,5 @@
+arr = [[:a, 1], ['b', 'two'], ['sea', {c: 3}], [{a: 1, b: 2, c: 3, d: 4}, 'D']]
+# expected return value: {:a=>1, "b"=>"two", "sea"=>{:c=>3}, {:a=>1, :b=>2, :c=>3, :d=>4}=>"D"}
+
+arr.map.with_object({}) |inner_arr, hash|
+  hash << inner_arr.map { |elements| element.first => element[1] }
