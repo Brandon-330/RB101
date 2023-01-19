fa7f570265127b061640ab64ef485aa6f2b096ca
Brandon Lima <112784850+Brandon-330@users.noreply.github.com>
2023-01-19 01:49:11 +0000

    new file:   lesson_6/.rubocop.yml   new file:   lesson_6/tic_tac_toe.rb

---
+++ lesson_6/tic_tac_toe.rb
@@ -0,0 +1,6 @@
+# Could use a hash, but there are 3 conditionals each square must go through
+# Why don't we use a hash to represent the picks? No, there are too many conditionals for a hash
+# We could do 
+# Let's use X for player, and O for computer
+
+tic_tac_toe = []
