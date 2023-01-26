cacc24a5b4f66619544760896bcee852f20f1dbf
Brandon Lima <112784850+Brandon-330@users.noreply.github.com>
2023-01-26 01:42:55 +0000

    new file:   small_problems/advanced_1/not_finished_exercise_4.rb

---
+++ small_problems/advanced_1/not_finished_exercise_4.rb
@@ -0,0 +1,17 @@
+def transpose(arr)
+  counter = 0
+  arr.map do |inner_arr| 
+    temp = []
+    1.times do |ind|
+      temp << arr[ind][counter]
+    end
+    counter += 1
+    temp
+  end
+end
+
+p transpose([[1, 2, 3, 4]]) == [[1], [2], [3], [4]]
+p transpose([[1], [2], [3], [4]]) == [[1, 2, 3, 4]]
+p transpose([[1, 2, 3, 4, 5], [4, 3, 2, 1, 0], [3, 7, 8, 6, 2]]) ==
+  [[1, 4, 3], [2, 3, 7], [3, 2, 8], [4, 1, 6], [5, 0, 2]]
+p transpose([[1]]) == [[1]]
