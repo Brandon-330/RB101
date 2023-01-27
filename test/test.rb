ccd199311afea38d7459c08f68a0e2c900e89493
Brandon Lima <112784850+Brandon-330@users.noreply.github.com>
2023-01-27 22:34:20 +0000

    new file:   small_problems/advanced_1/exercise_6.rb 	new file:   small_problems/advanced_1/not_finished_exercise_7.rb 	new file:   small_problems/advanced_1/not_finished_exercise_8.rb 	modified:   test/test.rb

--- test/test.rb
+++ test/test.rb
@@ -1,10 +1,14 @@
-user_input = gets
+def names_youngest_to_oldest(people)
+  names = people.keys
 
-loop do
-  name = user_input
-  break
+  names.sort_by { |name| people[name] }
 end
 
-if user_input
-  puts "Hello " + name
-end
+#Test Cases
+people_1 = { 'jim' => 50, 'jill' => 25, "artemis" => 42, 'johnny' => 37, 'earl' => 65 }
+people_2 = { 'alexandra' => 5, 'bob' => 94, "jolene" => 44, 'demosthenes' => 26 }
+people_3 = { 'sigmund' => 10, 'jane' => 21, "colin" => 17 }
+
+p names_youngest_to_oldest(people_1) == ['jill', 'johnny', 'artemis', 'jim', 'earl'] #=> true
+p names_youngest_to_oldest(people_2) == ['alexandra', 'demosthenes', 'jolene', 'bob'] #=> true
+p names_youngest_to_oldest(people_3) == ['sigmund', 'colin', 'jane'] #=> true
